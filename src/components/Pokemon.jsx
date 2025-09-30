b8cab357202e703981452d4b12c301b5ba88e461
Vinayak Patil <vinayakom@gmail.com>
2025-09-30 16:08:37 +0530

    [POKAPI-0006] - Enhance Pokemon component with loading and error handling states

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,6 +4,8 @@ import "./Pokemon.css"
 export const Pokemon = () => {
 
     const [pokemon, setPokemon] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
@@ -12,8 +14,13 @@ export const Pokemon = () => {
             .then((res) => res.json())
             .then((data) => {
                 setPokemon(data);
+                setLoading(false);
             })
-            .catch((error) => console.log(error))
+            .catch((error) => {
+                console.log(error);
+                setError(error);
+                setLoading(false);
+            });
     };
 
     useEffect(() => {
@@ -22,18 +29,26 @@ export const Pokemon = () => {
 
     console.log(pokemon);
 
-    if (!pokemon)
+    // Initial loading stage
+    if (loading)
         return (
             <div>
                 <h1>Loading....</h1>
             </div>
         );
 
+    // Handle error while initial loading
+    if (error)
+        return (
+            <div>
+                <h1>Error: {error.message}</h1>
+            </div>
+        );
 
     return (
         <section className="container">
             <header>
-                <h1>Lets catch Pokemon</h1>
+                <h1>Poke Poke Pokemon !!</h1>
             </header>
             <ul className="card-demo">
                 <li className="pokemon-card">
@@ -45,6 +60,17 @@ export const Pokemon = () => {
                         />
                     </figure>
                     <h1>{pokemon.name}</h1>
+                    <div className="grid-three-cols">
+                        <p className="pokemon-info">
+                            Height: <span> {pokemon.height} </span>
+                        </p>
+                        <p className="pokemon-info">
+                            Weight: <span> {pokemon.weight} </span>
+                        </p>
+                        <p className="pokemon-info">
+                            Speed: <span> {pokemon.stats[5].base_stat} </span>
+                        </p>
+                    </div>
                 </li>
             </ul>
         </section>
